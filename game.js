ad4df2811c4a71c5b0630edaa1eaecf3443aa796
D3nis1238 <158023075+D3nis1238@users.noreply.github.com>
2024-02-21 11:15:44 +0200

    AS

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 class Main extends Phaser.Scene {
-    // This function essentially loads things into our game
     preload() {
       this.load.spritesheet("rocket", "assets/31-312092_nasa-spaceship-clipart-page-4-pics-about-space.png", {
         frameWidth: 98,
@@ -8,16 +7,9 @@ class Main extends Phaser.Scene {
       this.load.image("meteorite", "assets/png-transparent-gray-asteroid-asteroid-sprite-asteroid-s-monochrome-rock-snout-thumbnail.png");
       this.load.audio("fly", "assets/kosmicheskaya-raketa-start-raketyi-29356.mp3");
     }
-
-    //  it runs once at the beginning of the game and
-    //  allows the user to place the things that they’ve preloaded with preload() and
-    //  create objects within our game such as animations, collision detectors, text, groups, and much more
     create() {
-      //Додаємо літак на сцену
       this.rocket = this.physics.add.sprite(0, 0, "rocket");
-      //Масштабуємо літак
       this.rocket.setScale(0.65, 1);
-      //Встановлюємо опорну точку літака
       this.rocket.setOrigin(0, 0.5);
       this.anims.create({
         key: "rocketAnimation",
@@ -29,7 +21,7 @@ class Main extends Phaser.Scene {
       });
       this.rocket.play("rocketAnimation");
 
-      this.rocket.body.gravity.y = 1000;
+      this.rocket.body.gravity.x = 1000;
       this.spaceBar = this.input.keyboard.addKey(
         Phaser.Input.Keyboard.KeyCodes.SPACE
       );
@@ -42,14 +34,13 @@ class Main extends Phaser.Scene {
 
       this.timedEvent = this.time.addEvent({
         delay: 1500,
-        callback: this.addRowOfMeteorites, //Цю функцію реалізуємо на наступному кроці
+        callback: this.addRowOfMeteorites, 
         callbackScope: this,
         loop: true,
       });
       this.physics.add.overlap(this.rocket, this.meteorites, this.hitMeteorite, null, this);
     }
 
-    // While preload() and create() run only once at the start of the game, update() runs constantly.
     update() {
       if (this.rocket.angle < 20) {
         this.rocket.angle += 1;
@@ -73,18 +64,16 @@ class Main extends Phaser.Scene {
       this.rocket.body.velocity.y = -350;
     }
 
-    //Функція для створення блоку труби
     addOnemeteorite(x, y) {
       var meteorite = this.physics.add.sprite(x, y, "meteorite");
       meteorite.setOrigin(0, 0);
-      this.meteorites.add(pipe);
-      meteorite.body.velocity.x = -300;
+      this.meteorites.add(meteorite);
+      meteorite.body.velocity.y = -300;
 
       meteorite.collideWorldBounds = true;
       meteorite.outOfBoundsKill = true;
     }
-    //Функція створення труби (стовпчик блоків)
-    addRowOfmeteorites() {
+    addRowOfMeteorites() {
       var hole = Math.floor(Math.random() * 5) + 1;
       this.score += 1;
       this.labelScore.text = this.score;
@@ -108,7 +97,7 @@ class Main extends Phaser.Scene {
     type: Phaser.AUTO,
     width: 400,
     height: 490,
-    scene: Main, // Цю сцену ми створимо на 4-му кроці
+    scene: Main, 
     backgroundColor: "#71c5cf",
     physics: {
       default: "arcade",
